a2ae02d19e255e9127c5a0f130294f780a8f6ff0
Tim Betker <tim.betker@imr.uni-hannover.de>
2019-01-24 13:31:42 +0100

    Added comment for threadcount

--- urs100_hardware_interface/src/urs100_hardware_interface_node.cpp
+++ urs100_hardware_interface/src/urs100_hardware_interface_node.cpp
@@ -4,6 +4,8 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "urs100_hardware_interface");
     ros::NodeHandle nh;
+    // Increment thread count if more actions/services/publishers/... are added to urs100
+    // See: https://answers.ros.org/question/313629/attempt-to-spin-a-callback-queue-from-two-spinners-one-of-them-being-single-threaded/?answer=313632#post-id-313632
     ros::AsyncSpinner spinner(2);
     urs100_hardware_interface::Urs100HardwareInterface Urs100Stage(nh);
     spinner.start();
