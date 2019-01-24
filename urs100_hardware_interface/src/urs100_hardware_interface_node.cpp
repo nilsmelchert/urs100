1c93884bd5df494b64ddc4143d6a635126e7e7c2
Tim Betker <tim.betker@imr.uni-hannover.de>
2019-01-24 13:23:37 +0100

    Removed spinner deprecation error by usin AsyncSpinner with 2 threads

--- urs100_hardware_interface/src/urs100_hardware_interface_node.cpp
+++ urs100_hardware_interface/src/urs100_hardware_interface_node.cpp
@@ -4,9 +4,10 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "urs100_hardware_interface");
     ros::NodeHandle nh;
-    ros::AsyncSpinner spinner(1);
-    spinner.start();
+    ros::AsyncSpinner spinner(2);
     urs100_hardware_interface::Urs100HardwareInterface Urs100Stage(nh);
+    spinner.start();
     ros::waitForShutdown();
+
     return 0;
 }
